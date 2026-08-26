75f64ed7e7702408d7f4445b420c14edb8fbff65
Abdurashid Ergashev <152396163+ergashev-dev@users.noreply.github.com>
2026-08-26 13:58:44 +0500

    Add CORS

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8000
-const connectDB = require('./config/db')
-const bookRoutes = require('./routes/bookRoutes')
-const userRoutes = require('./routes/userRoutes')
+require("dotenv").config();
 
-app.use(express.json())
+const express = require("express");
+const cors = require("cors");
 
-app.use('/api/books', bookRoutes)
-app.use('/api/users', userRoutes)
+const app = express();
 
+const PORT = process.env.PORT || 8000;
 
-connectDB().then(app.listen(PORT, () => console.log(`Server ${PORT}-portda ishga tushdi!`)))
+const connectDB = require("./config/db");
+const bookRoutes = require("./routes/bookRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/books", bookRoutes);
+app.use("/api/users", userRoutes);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server ${PORT}-portda ishga tushdi!`);
+  });
+});
